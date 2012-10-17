ecc16e2c7f4c9ea479e30d3ebc1938df063d642a
syslogic <martin@codefx.biz>
2012-10-17 18:29:43 +0200

    just some output modified.

--- pagoda/wp-install.php
+++ pagoda/wp-install.php
@@ -1,6 +1,6 @@
 <?php
 /*
-	WordPress Installer for PagodaBox v1.05
+	WordPress Installer for Pagoda Box v1.05
 	Copyright 2012 by Martin Zeitler
 	http://codefx.biz/contact
 */
@@ -29,7 +29,7 @@ if(file_exists($dst)){unlink($dst);}
 $fp = fopen($dst, 'w');
 $curl = curl_init();
 $opt = array(
-	CURLOPT_USERAGENT => 'WordPress Installer for PagodaBox',
+	CURLOPT_USERAGENT => 'WordPress Installer for Pagoda Box',
 	CURLOPT_URL => $src,
 	CURLOPT_HEADER => false,
 	CURLOPT_FILE => $fp
@@ -49,16 +49,17 @@ if(!file_exists($dst)){
 
 /* cURL stats */
 $time = $info['total_time']-$info['namelookup_time']-$info['connect_time']-$info['pretransfer_time']-$info['starttransfer_time']-$info['redirect_time'];
-echo "[cURL] fetched '$src' @ ".round(($info['size_download']*8/$time/1024/1024/1024),2)."GBps.\n";
+echo "[cURL] fetched '$src' @ ".abs(round(($info['size_download']*8/$time/1024/1024/1024),2))."GBps.\n";
 echo "[cURL] saved file to ".$dst.".\n";
 
 $zip = new ZipArchive;
 if($zip->open($dst) === TRUE) {
+	echo '[Zip] archive opened: '.$dst;
 	$zip->extractTo($base_dir, 'wordpress');
 	$zip->close();
 }
 else {
-	echo '[Zip] file open has failed: '.$dst;
+	echo '[Zip] archive could not be opened: '.$dst;
 }
 
 /* remove downloaded package */
